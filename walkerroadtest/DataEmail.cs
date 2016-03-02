428ce045fe84c4c6d72cf3f59cfd1797a19548d5
bgonzales20001 <bgonzales20001@gmail.com>
2016-03-01 17:24:39 -0800

    Testing

---
+++ walkerroadtest/DataEmail.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaseyaEpicorValidationVerification
+{
+    public class DataEmail
+    {
+
+        public const string email1 = @"From: Support_Desk [mailto:kaseya@rgsupport.com] 
+Sent: Thursday, January 28, 2016 10:44 AM
+To: Test Test <test@company.com>
+Subject: FOH006410 Closed
+
+Ticket FOH006410 has now been closed. If this issue has not been resolved to your satisfaction, please contact us so we can further assist you with this issue.
+ 
+Summary: Test Ticket Submitted
+Created on: 1/28/2016 10:20:54 AM 
+Status: Closed
+Organization: test-passport
+Machine Id: osi-laptop.test-passport
+Submitter Name: Test Test 
+Submitter Email: test@company.com 
+Assignee: rfunk  
+ 
+Notes:The station was not able to communicate over the Internet.  We rebooted the cell modem and communications were restored.
+________________________________________
+
+Updated ticket fields within kaseya.
+________________________________________
+
+";
+
+        public const string email2 = @"hqsmail2.company.com (10.33.140.22) with Microsoft SMTP Server (TLS) id
+ 15.0.1076.9 via Mailbox Transport; Tue, 1 Mar 2016 10:36:50 -0800
+Received: from hqsmail2.company.com (10.33.140.22) by
+ hqsmail1.company.com (10.33.140.21) with Microsoft SMTP Server (TLS) id
+ 15.0.1076.9; Tue, 1 Mar 2016 10:36:50 -0800
+Received: from hqsmail2.company.com ([fe80::99e:ec93:2797:faf0]) by
+ hqsmail2.company.com ([fe80::99e:ec93:2797:faf0%12]) with mapi id
+ 15.00.1076.000; Tue, 1 Mar 2016 10:36:50 -0800
+From: Ben Gonzales <bgonzales@company.com>
+To: Test <test@company.com>
+Subject: FOH006411 Closed
+Thread-Topic: FOH006411 Closed
+Thread-Index: AdFz6UxAK2q7GYo6TJK9Mb4/OhWD1Q==
+Date: Tue, 1 Mar 2016 10:36:49 -0800
+Message-ID: <10568c37818148668ab8e1e7e8ff6ddb@hqsmail2.company.com>
+Accept-Language: en-US
+Content-Language: en-US
+X-MS-Exchange-Organization-AuthAs: Internal
+X-MS-Exchange-Organization-AuthMechanism: 04
+X-MS-Exchange-Organization-AuthSource: hqsmail2.company.com
+X-MS-Has-Attach:
+X-MS-Exchange-Organization-SCL: -1
+X-MS-TNEF-Correlator:
+Content-Type: text/plain; charset=""us-ascii""
+MIME-Version: 1.0
+
+Ticket FOH006411 has now been closed. If thReceived: from hqsmail1.company.com (10.33.140.21) by
+is issue has not been resolved to your satisfaction, please contact us so we can further assist you with this issue.
+
+Summary: Kaseya Ticket Submitted
+Created on: 1/28/2016 10:20:54 AM
+EpicorCaseNum: 21355
+Status: Closed
+Organization: company-passport
+Machine Id: osi-laptop.company-passport
+Submitter Name: Test2 User2
+Submitter Email: testuser2@company.com
+Assignee: rfunk
+
+Notes:The station was not able to communicate over the Internet.  We rebooted the cell modem and communications were restored.
+________________________________________
+
+Updated ticket fields within kaseya.
+________________________________________";
+
+    }
+}
---
+++ walkerroadtest/PopControllerTest.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
+using System.Collections.Generic;
+
+using jacobsenroad;
+
+namespace KaseyaEpicorValidationVerification
+{
+    [TestClass]
+    public class PopControllerTest
+    {
+        private struct attr
+        {
+            public string name;
+            public string value;
+        }
+
+        private List<attr> props = new List<attr>();
+
+        [TestMethod]
+        public void TestKaseyaEpicorController()
+        {
+            PopClientController controller;
+            controller = new PopClientController();
+            controller.Run();
+        }
+
+        [TestMethod]
+        public void TestScanner_AcceptRun1()
+        {
+            Scanner scanner = new Scanner();
+
+            scanner.Input(DataEmail.email2);
+
+            List<byte> token;
+            string tok1;
+            attr atr;
+
+            token = scanner.AcceptRun(ASCIIEncoding.ASCII.GetBytes(".0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"));
+            tok1 = ASCIIEncoding.ASCII.GetString(token.ToArray());
+            if(tok1.Equals("hqsmail2.eidpassport.com"))
+            {
+                token = scanner.AcceptUntil(ASCIIEncoding.ASCII.GetBytes("\r\n"));
+                tok1 = ASCIIEncoding.ASCII.GetString(token.ToArray());
+
+                bool found = false;
+                while (!found) 
+                {
+                    scanner.SkipEmpty();
+
+                    token = scanner.AcceptRun(ASCIIEncoding.ASCII.GetBytes("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"));
+                    tok1 = ASCIIEncoding.ASCII.GetString(token.ToArray());
+
+                    if (tok1.Equals("From"))
+                    {
+                        found = true;
+                    }
+                    else
+                    {
+                        token = scanner.AcceptUntil(ASCIIEncoding.ASCII.GetBytes("\r\n"));
+                        string tok2 = ASCIIEncoding.ASCII.GetString(token.ToArray());
+                    }
+                }
+
+                token = scanner.AcceptRun(ASCIIEncoding.ASCII.GetBytes(":"));
+                tok1 = ASCIIEncoding.ASCII.GetString(token.ToArray());
+                if (tok1.Equals(":"))
+                {
+                    scanner.SkipEmpty();
+                    token = scanner.AcceptUntil(ASCIIEncoding.ASCII.GetBytes("<"));
+                    tok1 = ASCIIEncoding.ASCII.GetString(token.ToArray());
+                    scanner.SkipEmpty();
+                    token = scanner.AcceptRun(ASCIIEncoding.ASCII.GetBytes("<"));
+                    tok1 = ASCIIEncoding.ASCII.GetString(token.ToArray());
+                    scanner.SkipEmpty();
+                    token = scanner.AcceptRun(ASCIIEncoding.ASCII.GetBytes("@.0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"));
+                    tok1 = ASCIIEncoding.ASCII.GetString(token.ToArray());
+                    scanner.SkipEmpty();
+                    token = scanner.AcceptRun(ASCIIEncoding.ASCII.GetBytes(">"));
+                    tok1 = ASCIIEncoding.ASCII.GetString(token.ToArray());
+                    scanner.SkipEmpty();
+                }
+                bool inLoop = true;
+                while (inLoop)
+                {
+                    scanner.SkipEmpty();
+                    token = scanner.AcceptRun(ASCIIEncoding.ASCII.GetBytes("-ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz "));
+                    tok1 = ASCIIEncoding.ASCII.GetString(token.ToArray());
+                    if (tok1.Equals("To"))
+                    {
+                        atr = new attr();
+                        string tokenName = tok1;
+                        atr.name = tokenName;
+                        scanner.SkipEmpty();
+                        token = scanner.AcceptRun(ASCIIEncoding.ASCII.GetBytes(":"));
+                        tok1 = ASCIIEncoding.ASCII.GetString(token.ToArray());
+                        scanner.SkipEmpty();
+                        token = scanner.AcceptUntil(ASCIIEncoding.ASCII.GetBytes("<"));
+                        tok1 = ASCIIEncoding.ASCII.GetString(token.ToArray());
+                        scanner.SkipEmpty();
+                        token = scanner.AcceptRun(ASCIIEncoding.ASCII.GetBytes("<"));
+                        tok1 = ASCIIEncoding.ASCII.GetString(token.ToArray());
+                        scanner.SkipEmpty();
+                        token = scanner.AcceptRun(ASCIIEncoding.ASCII.GetBytes("@.0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"));
+                        tok1 = ASCIIEncoding.ASCII.GetString(token.ToArray());
+                        atr.value = tok1;
+                        props.Add(atr);
+                        scanner.SkipEmpty();
+                        token = scanner.AcceptRun(ASCIIEncoding.ASCII.GetBytes(">"));
+                        tok1 = ASCIIEncoding.ASCII.GetString(token.ToArray());
+                        scanner.SkipEmpty();
+                    }
+                    else if (tok1.Equals("Subject"))
+                    {
+                        atr = new attr();
+                        string tokenName = tok1;
+                        atr.name = tokenName;
+                        scanner.SkipEmpty();
+                        token = scanner.AcceptRun(ASCIIEncoding.ASCII.GetBytes(":"));
+                        tok1 = ASCIIEncoding.ASCII.GetString(token.ToArray());
+                        scanner.SkipEmpty();
+                        token = scanner.AcceptUntil(ASCIIEncoding.ASCII.GetBytes("\r\n"));
+                        tok1 = ASCIIEncoding.ASCII.GetString(token.ToArray());
+                        atr.value = tok1;
+                        props.Add(atr);
+                        scanner.SkipEmpty();
+                    }
+                    else if (tok1.Equals("MIME-Version"))
+                    {
+                        atr = new attr();
+                        string tokenName = tok1;
+                        atr.name = tokenName;
+                        scanner.SkipEmpty();
+                        token = scanner.AcceptRun(ASCIIEncoding.ASCII.GetBytes(":"));
+                        tok1 = ASCIIEncoding.ASCII.GetString(token.ToArray());
+                        scanner.SkipEmpty();
+                        token = scanner.AcceptUntil(ASCIIEncoding.ASCII.GetBytes("\r\n"));
+                        tok1 = ASCIIEncoding.ASCII.GetString(token.ToArray());
+                        atr.value = tok1;
+                        props.Add(atr);
+                        scanner.SkipEmpty();
+                        //inLoop = false;
+                    }
+                    else if (tok1.Equals("Content-Type"))
+                    {
+                        atr = new attr();
+                        string tokenName = tok1;
+                        atr.name = tokenName;
+                        scanner.SkipEmpty();
+                        token = scanner.AcceptRun(ASCIIEncoding.ASCII.GetBytes(":"));
+                        tok1 = ASCIIEncoding.ASCII.GetString(token.ToArray());
+                        scanner.SkipEmpty();
+                        token = scanner.AcceptUntil(ASCIIEncoding.ASCII.GetBytes("\r\n"));
+                        tok1 = ASCIIEncoding.ASCII.GetString(token.ToArray());
+                        atr.value = tok1;
+                        props.Add(atr);
+                        scanner.SkipEmpty();
+                    }
+                    else if (tok1.Length == 0)
+                    {
+                        inLoop = false;
+                    }
+                    else
+                    {
+                        atr = new attr();
+                        string tokenName = tok1;
+                        atr.name = tokenName;
+                        scanner.SkipEmpty();
+                        token = scanner.AcceptRun(ASCIIEncoding.ASCII.GetBytes(":"));
+                        tok1 = ASCIIEncoding.ASCII.GetString(token.ToArray());
+                        scanner.SkipEmpty();
+                        token = scanner.AcceptUntil(ASCIIEncoding.ASCII.GetBytes("\r\n"));
+                        string tokenValue = ASCIIEncoding.ASCII.GetString(token.ToArray());
+                        atr.value = tokenValue;
+                        scanner.SkipEmpty();
+                        props.Add(atr);
+                    }
+                }
+            }
+
+        }
+
+        [TestMethod]
+        public void TestScanner_AcceptRun2()
+        {
+            Scanner scanner = new Scanner();
+
+            scanner.Input(DataEmail.email1);
+
+            List<byte> token;
+            string tok1;
+            attr atr;
+
+            for (int i = 0; i < 4; i++)
+            {
+                atr = new attr();
+
+                token = scanner.AcceptRun(ASCIIEncoding.ASCII.GetBytes("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"));
+                tok1 = ASCIIEncoding.ASCII.GetString(token.ToArray());
+
+                atr.name = tok1;
+
+                scanner.SkipEmpty();
+                token = scanner.AcceptRun(ASCIIEncoding.ASCII.GetBytes(":"));
+                tok1 = ASCIIEncoding.ASCII.GetString(token.ToArray());
+
+                scanner.SkipEmpty();
+                token = scanner.AcceptUntil(ASCIIEncoding.ASCII.GetBytes("\r\n"));
+                tok1 = ASCIIEncoding.ASCII.GetString(token.ToArray());
+                atr.value = tok1;
+
+                props.Add(atr);
+
+                token = scanner.AcceptRun(ASCIIEncoding.ASCII.GetBytes("\r"));
+                tok1 = ASCIIEncoding.ASCII.GetString(token.ToArray());
+                token = scanner.AcceptRun(ASCIIEncoding.ASCII.GetBytes("\n"));
+                tok1 = ASCIIEncoding.ASCII.GetString(token.ToArray());
+                scanner.SkipEmpty();
+            }
+            token = scanner.AcceptRun(ASCIIEncoding.ASCII.GetBytes("\r"));
+            tok1 = ASCIIEncoding.ASCII.GetString(token.ToArray());
+            token = scanner.AcceptRun(ASCIIEncoding.ASCII.GetBytes("\n"));
+            tok1 = ASCIIEncoding.ASCII.GetString(token.ToArray());
+            //token = scanner.AcceptRun(ASCIIEncoding.ASCII.GetBytes("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.,;_$% "));
+            token = scanner.AcceptUntil(ASCIIEncoding.ASCII.GetBytes("\r\n"));
+            tok1 = ASCIIEncoding.ASCII.GetString(token.ToArray());
+            scanner.SkipEmpty();
+
+            //------------------
+
+            for (int i = 0; i < 9; i++)
+            {
+                atr = new attr();
+                token = scanner.AcceptUntil(ASCIIEncoding.ASCII.GetBytes(":"));
+                tok1 = ASCIIEncoding.ASCII.GetString(token.ToArray());
+
+                atr.name = tok1;
+
+                scanner.SkipEmpty();
+                token = scanner.AcceptRun(ASCIIEncoding.ASCII.GetBytes(":"));
+                tok1 = ASCIIEncoding.ASCII.GetString(token.ToArray());
+
+                scanner.SkipEmpty();
+                token = scanner.AcceptUntil(ASCIIEncoding.ASCII.GetBytes("\r\n"));
+                tok1 = ASCIIEncoding.ASCII.GetString(token.ToArray());
+                atr.value = tok1;
+
+                props.Add(atr);
+                scanner.SkipEmpty();
+            }
+        }
+    }
+}
